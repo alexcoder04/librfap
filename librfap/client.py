7d6a43f56e5bba4e252b532c03949542e88c8365
alexcoder04 <ailyushonak@gmail.com>
2021-12-09 17:23:07 +0100

    complete splitting

--- librfap/client.py
+++ librfap/client.py
@@ -14,6 +14,7 @@ class Client:
         self.SERVER_ADDRESS = server_address
         self.PORT = port
         self.WAIT_FOR_RESPONSE = 0.1
+        self.MAX_CONTENT_SIZE = 16*1024*1024
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         try:
             self.socket.connect((self.SERVER_ADDRESS, self.PORT))
@@ -22,7 +23,7 @@ class Client:
             sys.exit(1)
 
     # BASIC FUNCTIONS
-    def send_command(self, command: int, metadata: dict, body: bytes = None) -> None:
+    def _send_command(self, command: int, metadata: dict, body: bytes = None) -> None:
         all_data = b""
         # version
         all_data += self.int_to_bytes(self.VERSION, 2)
@@ -47,7 +48,7 @@ class Client:
         # send everything
         self.socket.send(all_data)
 
-    def recv_command(self):
+    def _recv_command(self):
         # receive everything
         data = self.socket.recv(2+4+(16*1024*1024)+4+(16*1024*1024))
 
@@ -76,6 +77,48 @@ class Client:
 
         return version, command, metadata, header_checksum, body, body_checksum
 
+    def recv_data(self):
+        first_version, first_command, first_metadata, _, body, _ = self._recv_command()
+        if first_metadata["PacketsTotal"] < 1:
+            raise Exception("invalid total packets number")
+        if first_metadata["PacketsTotal"] == 1:
+            return first_version, first_command, first_metadata, body
+        for i in range(first_metadata["PacketsTotal"] - 1):
+            this_version, this_command, this_metadata, _, body_part, _ = self._recv_command()
+            if this_version != first_version:
+                raise Exception("Data in different packets does not match")
+            if this_metadata["PacketNumber"] != i+1:
+                raise Exception("Data in different packets does not match")
+            if this_command != first_command:
+                raise Exception("Data in different packets does not match")
+            # TODO check if other metadata values match
+            body = body + body_part
+        return first_version, first_command, first_metadata, body
+
+    def send_data(self, command: int, metadata: dict, body: bytes = None) -> None:
+        if body is None:
+            metadata["PacketNumber"] = 1
+            metadata["PacketsTotal"] = 1
+            self._send_command(command, metadata, body)
+            return
+        if len(body) <= self.MAX_CONTENT_SIZE:
+            metadata["PacketNumber"] = 1
+            metadata["PacketsTotal"] = 1
+            self._send_command(command, metadata, body)
+            return
+        i = 0
+        packets = []
+        while True:
+            if (i + self.MAX_CONTENT_SIZE) >= len(body):
+                packets.append((command, metadata, body[i:]))
+                break
+            packets.append((command, metadata, body[i : (i+self.MAX_CONTENT_SIZE)]))
+            i += self.MAX_CONTENT_SIZE
+        metadata["PacketsTotal"] = len(packets)
+        for i, p in enumerate(packets):
+            metadata["PacketNumber"] = i + 1
+            self._send_command(*p)
+
     # HELPER FUNCTIONS
     def int_to_bytes(self, value: int, bytes_number: int = 4) -> bytes:
         return value.to_bytes(bytes_number, self.ENDIANESS)
@@ -85,12 +128,12 @@ class Client:
 
     # IMPLEMENTATION OF RFAP COMMANDS
     def rfap_ping(self) -> None:
-        self.send_command(CMD_PING, {})
+        self.send_data(CMD_PING, {})
         time.sleep(self.WAIT_FOR_RESPONSE)
-        self.recv_command()
+        self.recv_data()
 
     def rfap_disconnect(self) -> None:
-        self.send_command(CMD_DISCONNECT, {})
+        self.send_data(CMD_DISCONNECT, {})
         time.sleep(self.WAIT_FOR_RESPONSE)
         self.socket.close()
 
@@ -99,15 +142,15 @@ class Client:
             requireDetails = ["DirectorySize", "ElementsNumber"]
         else:
             requireDetails = []
-        self.send_command(CMD_INFO, {"Path": path, "RequestDetails": requireDetails})
+        self.send_data(CMD_INFO, {"Path": path, "RequestDetails": requireDetails})
         time.sleep(self.WAIT_FOR_RESPONSE)
-        _, _, metadata, _, _, _ = self.recv_command()
+        _, _, metadata, _, = self.recv_data()
         return metadata
 
     def rfap_file_read(self, path: str):
-        self.send_command(CMD_FILE_READ, {"Path": path})
+        self.send_data(CMD_FILE_READ, {"Path": path})
         time.sleep(self.WAIT_FOR_RESPONSE)
-        _, _, metadata, _, body, _ = self.recv_command()
+        _, _, metadata, body = self.recv_data()
         if metadata["ErrorCode"] != 0:
             return metadata, b""
         return metadata, body
@@ -117,9 +160,9 @@ class Client:
             requireDetails = ["DirectorySize", "ElementsNumber"]
         else:
             requireDetails = []
-        self.send_command(CMD_DIRECTORY_READ, {"Path": path, "RequestDetails": requireDetails})
+        self.send_data(CMD_DIRECTORY_READ, {"Path": path, "RequestDetails": requireDetails})
         time.sleep(self.WAIT_FOR_RESPONSE)
-        _, _, metadata, _, body, _ = self.recv_command()
+        _, _, metadata, body = self.recv_data()
         if metadata["ErrorCode"] != 0:
             return metadata, []
         return metadata, [i for i in body.decode("utf-8").split("\n") if i != ""]
