051662fe101580599ae14cd8f82911fbb6edd952
alexcoder04 <ailyushonak@gmail.com>
2021-12-17 15:08:08 +0100

    update pip build info

--- setup.py
+++ setup.py
@@ -1,17 +1,35 @@
-#!/usr/bin/env python3
+from setuptools import setup, find_packages
+import pathlib
 
-from setuptools import setup
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
-        name="librfap",
-        version="pre-alpha",
-        description="Python client library for rfap",
-        url="https://github.com/alexcoder04/librfap",
-        license="GNU GPLv3",
-        packages=["librfap"],
-        install_requires=[
-            "setuptools",
-            "pyyaml"
-            ]
-        )
+    name='librfap',
+    version='0.2.0',
+    description='client library for rfap',
+    long_description=long_description,  # Optional
+    long_description_content_type='text/markdown',  # Optional (see note above)
+    url='https://github.com/alexcoder04/librfap',
+    license="GNU GPLv3",
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        "Programming Language :: Python :: 3.10",
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    package_dir={'': 'librfap'},
+    packages=find_packages(where='src'),  # Required
+    python_requires='>=3.6, <4',
+    install_requires=['pyyaml'],
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/alexcoder04/librfap/issues',
+        'Source': 'https://github.com/alexcoder04/librfap/',
+    },
+)
 
