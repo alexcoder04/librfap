a356f8bc80e212b64224952b557ba80160ac3761
alexcoder04 <ailyushonak@gmail.com>
2021-12-17 15:39:24 +0100

    setup stuff

--- setup.py
+++ setup.py
@@ -3,12 +3,7 @@
 from setuptools import setup
 
 setup(
-        name="librfap",
-        version="pre-alpha",
-        description="Python client library for rfap",
-        url="https://github.com/alexcoder04/librfap",
-        license="GNU GPLv3",
-        packages=["librfap"],
+        version="0.2.0",
         install_requires=[
             "setuptools",
             "pyyaml"
